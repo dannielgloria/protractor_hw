2fafce659328cd228e6ac8637e0d86f9307d88e4
Daniel Gloria <danniel.gloria@gmail.com>
2021-01-16 16:33:16 -0600

    Protractor - DEMO(sum)

--- calculator.js
+++ calculator.js
@@ -1,9 +1,34 @@
 // spec.js
 describe('Protractor Demo App - Calculator from http://juliemr.github.io/protractor-demo/', function() {
-    it('should have a title', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
 
+    const urlProyect = { url: "http://juliemr.github.io/protractor-demo/" }; // url del proyecto
+
+    it('It should have a title', function() {
+        browser.get(urlProyect.url);
         expect(browser.getTitle()).toEqual('Super Calculator');
+        browser.sleep(1000);
+    });
+
+    it('It should have to add', function() {
+        browser.get(urlProyect.url);
+        let input_one = element(by.model('first'));
+        let input_two = element(by.model('second'));
+        let button = element(by.css('[ng-click="doAddition()"]'));
+
+        input_one.sendKeys('2');
+        input_two.sendKeys('3');
+        button.click();
+
+        browser.sleep(2000);
+    });
+
+    it('It should show the correct result of the sum', function() {
+        browser.get(urlProyect.url);
+
+        let result = element(by.cssContainingText('.ng-binding', '5'));
+
+        expect(result.getText().toEqual('10'));
+
         browser.sleep(2000);
     });
 });
