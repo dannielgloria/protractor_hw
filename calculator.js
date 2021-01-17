c4b4b9db269259b5409ab4202856dacc1af4e89a
Daniel Gloria <danniel.gloria@gmail.com>
2021-01-16 22:43:50 -0600

    Protractor - DEMO(allOperations)

--- calculator.js
+++ calculator.js
@@ -10,18 +10,18 @@ describe('Protractor Demo App - Calculator from http://juliemr.github.io/protrac
         homepage.enterFirstValueNumber(firstNumber_Value = '10');
         homepage.enterSecondValueNumber(secondNumber_Value = '10');
         homepage.clickGoButton();
-        homepage.verifyResult(result = '21');
+        homepage.verifyResult(result = '20');
         browser.sleep(2000);
     });
 
-    it('It should show the correct result of the division', function() {
+    it('It should show the correct result of the operation', function() {
         homepage.getUrlProyect(urlProyect.url);
         homepage.enterFirstValueNumber(firstNumber_Value = '10');
         homepage.enterSecondValueNumber(secondNumber_Value = '2');
         homepage.clickOperator();
-        homepage.selectOperator();
+        homepage.selectOperator(operator = "MULTIPLICATION");
         homepage.clickGoButton();
-        homepage.verifyResult(result = '5');
+        homepage.verifyResult(result = '20');
         browser.sleep(2000);
     });
 
--- page/homepage.js
+++ page/homepage.js
@@ -2,7 +2,13 @@ let HomePage = function() {
     let firstNumber_input = element(by.model('first'));
     let secondNumber_input = element(by.model('second'));
     let operator = element(by.model('operator'));
-    let selectOperation = element(by.xpath(".//option[@value= 'DIVISION']"));
+    let selectOperationModulo = element(by.xpath(".//option[@value= 'MODULO']"));
+    let selectOperationDivision = element(by.xpath(".//option[@value= 'DIVISION']"));
+    let selectOperationAddition = element(by.xpath(".//option[@value= 'ADDITION']"));
+    let selectOperationSubstraction = element(by.xpath(".//option[@value= 'SUBTRACTION']"));
+    let selectOperationMultiplication = element(by.xpath(".//option[@value= 'MULTIPLICATION']"));
+
+
     let goButton = element(by.css('[ng-click="doAddition()"]'));
 
     this.getUrlProyect = function(url) {
@@ -21,8 +27,36 @@ let HomePage = function() {
         operator.click();
     };
 
+
     this.selectOperator = function() {
         selectOperation.click();
+    };
+
+    this.selectOperator = function(operator) {
+        switch (operator) {
+            case "MODULO":
+                selectOperationModulo.click();
+                break;
+
+            case "DIVISION":
+                selectOperationDivision.click();
+                break;
+
+            case "ADDITION":
+                selectOperationAddition.click();
+                break;
+
+            case "SUBTRACTION":
+                selectOperationSubstraction.click();
+                break;
+
+            case "MULTIPLICATION":
+                selectOperationMultiplication.click();
+                break;
+
+            default:
+                text = "Unidentified operation";
+        }
     };
 
     this.clickGoButton = function() {
